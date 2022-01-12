95b99c829bfcc6021b2a9ab704bfaedfc4b453bc
Sebastian Apel <marloto@codestack.de>
2022-01-12 11:55:18 +0100

    added ajax examples

---
+++ pruefungsuebungen/something.php
@@ -0,0 +1,5 @@
+<?php
+$msg = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, vero? Test!";
+$data = array("text" =>  $msg);
+echo json_encode($data);
+?>
---
+++ pruefungsuebungen/something2.php
@@ -0,0 +1,2 @@
+<?php $msg = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, vero? Test!"; ?>
+<p><?= $msg ?></p>
